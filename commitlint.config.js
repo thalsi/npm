66b95e7e4528c468032abec3439588132f4f067a
thalsi <thalseehthalsi609@gmail.com>
2025-07-03 01:21:08 +0530

    test: husky lint-staged eslint prettier commitlint

---
+++ commitlint.config.js
@@ -0,0 +1,23 @@
+// commitlint.config.js
+export default {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "type-enum": [
+      2,
+      "always",
+      [
+        "feat",
+        "fix",
+        "docs",
+        "style",
+        "refactor",
+        "perf",
+        "test",
+        "build",
+        "ci",
+        "chore",
+        "revert",
+      ],
+    ],
+  },
+};
